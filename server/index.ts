d7895c81ca50a2d093c04123ef905b19a53e0ae7
Jagjeevan Kashid <jagjeevandev97@gmail.com>
2025-08-01 02:11:49 +0530

    vercel
    Signed-off-by: Jagjeevan Kashid <jagjeevandev97@gmail.com>

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import connectDB from "./config/database.js";
+import connectDB from "./config/database";
 import { 
   Student, 
   Candidate, 
